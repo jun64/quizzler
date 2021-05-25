118df52a9b409a78d0d5d358d039a9d0a5d73aa5
jun64 <mahaha94@gmail.com>
2021-05-25 18:39:23 +0900

    quizzler app 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,44 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [
+    // List<Icon> works too
+  ];
+
+  int score = 0;
+
+  void checkAnswer(bool userPickedAnswer) {
+    if (quizBrain.isFinished()){
+      Alert(context: context, title: "문제의 끝입니다.", desc: "your score: $score").show();
+      score = 0;
+      setState(() {
+        scoreKeeper.clear();
+      });
+    } else {
+      bool correctAnswer = quizBrain.getAnswer();
+
+      setState(() {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(
+              Icon(
+                Icons.check,
+                color: Colors.green,
+              )
+          );
+          score++;
+        } else {
+          scoreKeeper.add(
+              Icon(
+                Icons.close,
+                color: Colors.red,
+              )
+          );
+        }
+        quizBrain.nextQuestion();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +79,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +104,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -80,11 +123,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
@@ -95,3 +141,10 @@ question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
 */
+
+// VCS -> local history
+
+// abstraction 추상화
+// encapsulation 캡슐화  _앞에 붙이면 프라이빗
+// inheritance 상속성  extends
+// polymorphism 다형성  extends @override super
---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+class Question {
+
+  String questionText;
+  bool questionAnswer;
+
+  Question({String q, bool a}){
+    //constructor
+    questionText = q;
+    questionAnswer = a;
+
+  }
+
+}
